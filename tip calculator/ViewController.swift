e3a4a59530b37c935e0af04fdd9ee30ad1448837
Atef Kai Benothman <kaiben312@gmail.com>
2019-03-23 17:44:47 -0700

    Initial Commit

---
+++ tip calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip calculator
+//
+//  Created by Atef Kai Benothman on 3/23/19.
+//  Copyright © 2019 Atef Kai Benothman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
